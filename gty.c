d8431b3e260629e98062c536a333430fced7b636
NAKAMURA Takumi <geek4civic@gmail.com>
2011-04-29 18:11:37 +0900

    Add dec64.

--- gty.c
+++ gty.c
@@ -3,6 +3,28 @@
 #include <inttypes.h>
 #include "sha1_32.h"
 
+static
+void dec64(char *h5, uint32_t h)
+{
+	char const m64[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./";
+	h >>= 2; h5[4] = m64[h & 63];
+	h >>= 6; h5[3] = m64[h & 63];
+	h >>= 6; h5[2] = m64[h & 63];
+	h >>= 6; h5[1] = m64[h & 63];
+	h >>= 6; h5[0] = m64[h & 63];
+}
+
+static
+void print_key_hash(FILE *fp, const uint32_t *h)
+{
+	char hstr[32];
+	dec64(&hstr[ 0], h[0]);
+	dec64(&hstr[ 5], (h[0] << 30) | (h[1] >> 2));
+	dec64(&hstr[10], (h[1] << 28) | (h[2] >> 4));
+	hstr[12] = 0;
+	fprintf(fp, "<%s>\n", hstr);
+}
+
 int main()
 {
 	static uint32_t h[5];
@@ -16,6 +38,7 @@ int main()
 	printf("7CA678DA 749193EC 305EE9A0 5E5BD477 8AD4F786: should be\n"
 		   "%08X %08X %08X %08X %08X\n",
 		   h[0], h[1], h[2], h[3], h[4]);
+	print_key_hash(stdout, h);
 
 	return 0;
 }
